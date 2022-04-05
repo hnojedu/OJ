e125cbf180dc659c0598f5e8cb1b5c4df440cbc4
Duc Bui <buianhduc.popodoo2@gmail.com>
2022-04-05 16:17:58 +0700

    change accent color, header background, and buttons

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '5*9f5q57mqmlz2#f$x1h76&jxy#yortjl1v+l*6hd18$d*yx#0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 CSRF_FAILURE_VIEW = 'judge.views.widgets.csrf_failure'
 
